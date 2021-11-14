f12165a1559dd44c6b9deff4b882648bab06fe20
Tianwei Yin <yintianwei@utexas.edu>
2021-11-14 17:26:04 -0600

    Update create_data.py

--- tools/create_data.py
+++ tools/create_data.py
@@ -8,7 +8,7 @@ from det3d.datasets.nuscenes import nusc_common as nu_ds
 from det3d.datasets.utils.create_gt_database import create_groundtruth_database
 from det3d.datasets.waymo import waymo_common as waymo_ds
 
-def nuscenes_data_prep(root_path, version, nsweeps=10, filter_zero=True):
+def nuscenes_data_prep(root_path, version, nsweeps=10, filter_zero=True, virtual=False):
     nu_ds.create_nuscenes_infos(root_path, version=version, nsweeps=nsweeps, filter_zero=filter_zero)
     if version == 'v1.0-trainval':
         create_groundtruth_database(
@@ -16,6 +16,7 @@ def nuscenes_data_prep(root_path, version, nsweeps=10, filter_zero=True):
             root_path,
             Path(root_path) / "infos_train_{:02d}sweeps_withvelo_filter_{}.pkl".format(nsweeps, filter_zero),
             nsweeps=nsweeps,
+            virtual=virtual
         )
 
 def waymo_data_prep(root_path, split, nsweeps=1):
